84d7fa96610defb8ebfd221fd28561a72ffe1e43
Khrystyna Derenivska <khrisderenivska@gmail.com>
2019-10-11 12:10:55 +0300

    Update map position

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let mymap = L.map("map").setView([51.505, -0.09], 13);
+let mymap = L.map("map").setView([47, 18], 4);
 
 L.tileLayer(
   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
